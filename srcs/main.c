0572a71057657e9add9d9f2ad3d540479f0133ee
larocqueg <portugalgabriel2021@gmail.com>
2025-05-20 17:40:56 +0100

    pong in c almost perfect

--- srcs/main.c
+++ srcs/main.c
@@ -12,23 +12,45 @@
 
 #include "../includes/cub.h"
 
+t_cub	cub;
 t_player player;
 t_player player2;
 t_player ball;
 t_point padsize = {20, 100};
-t_point ballsize = {15, 15};
-float	ball_speed = 300;
+t_point ballsize = {16, 16};
+char	score_1[] = "0";
+char	score_2[] = "0";
+bool	hit_left = true;
+bool	hit_right = true;
+float	ball_speed = 350;
 float	ball_accel = 500;
 bool	started = false;
 float	speed = 500;
 float delta = 0;
-float gravity = 10;
-float accel = 500;
-float friction = 500;
+float accel = 1000;
+float friction = 1000;
+float offset = 40;
 long long last_frame_time = 0;
 
 
 int	game_loop(t_cub *cub);
+void init_players();
+
+t_point	normalize(t_point point)
+{
+	float len = sqrtf(point.x * point.x + point.y * point.y);
+	len = sqrtf(len);
+
+	if (len == 0)
+		return ((t_point){0, 0});
+	else
+	{
+		point.x /= len;
+		point.y /= len;
+		return (point);
+	}
+}
+
 void	drawrect(t_image *image, t_point pos, t_point size, int color)
 {
 	int	x = 0;
@@ -54,10 +76,26 @@ float clamp(float value, float min, float max)
 	return value;
 }
 
+void	get_random()
+{
+	ball.direction.x = (float)rand() / (float)RAND_MAX * 2.0f - 1.0f;
+	ball.direction.y = (float)rand() / (float)RAND_MAX * 1.4f - 0.7f;
+	if (ball.direction.x > 0)
+		ball.direction.x = 1;
+	else
+		ball.direction.x = -1;
+	ball.direction = normalize(ball.direction);
+}
 
 int	key_press(int keysym, t_cub *cub)
 {
 	(void)cub;
+	if (keysym == XK_space && !started)
+	{
+		get_random();
+		ball_speed = 350;
+		started = true;
+	}
 	if (keysym == XK_Escape)
 	{
 		free_displays(cub);
@@ -65,24 +103,24 @@ int	key_press(int keysym, t_cub *cub)
 	if (keysym == XK_W || keysym == XK_w)
 	{
 		player.direction.y += -1;
-		player.direction.x = clamp(player.direction.x, -1, 1);
+		player.direction.y = clamp(player.direction.y, -1, 1);
 
 	}
 	if (keysym == XK_S || keysym == XK_s)
 	{
 		player.direction.y += 1;
-		player.direction.x = clamp(player.direction.x, -1, 1);
+		player.direction.y = clamp(player.direction.y, -1, 1);
 	}
 	if (keysym ==  XK_Up)
 	{
 		player2.direction.y += -1;
-		player2.direction.x = clamp(player2.direction.x, -1, 1);
+		player2.direction.y = clamp(player2.direction.y, -1, 1);
 
 	}
 	if (keysym == XK_Down)
 	{
 		player2.direction.y += 1;
-		player2.direction.x = clamp(player2.direction.x, -1, 1);
+		player2.direction.y = clamp(player2.direction.y, -1, 1);
 	}
 	return (0);
 }
@@ -98,26 +136,26 @@ int	key_release(int keysym, t_cub *cub)
 	if (keysym == XK_W || keysym == XK_w)
 	{
 		player.direction.y += 1;
-		player.direction.x = clamp(player.direction.x, -1, 1);
+		player.direction.y = clamp(player.direction.y, -1, 1);
 
 	}
 	if (keysym == XK_S || keysym == XK_s)
 	{
 		player.direction.y += -1;
-		player.direction.x = clamp(player.direction.x, -1, 1);
+		player.direction.y = clamp(player.direction.y, -1, 1);
 
 	}
 
 	if (keysym == XK_Up)
 	{
 		player2.direction.y += 1;
-		player2.direction.x = clamp(player2.direction.x, -1, 1);
+		player2.direction.y = clamp(player2.direction.y, -1, 1);
 
 	}
 	if (keysym == XK_Down)
 	{
 		player2.direction.y += -1;
-		player2.direction.x = clamp(player2.direction.x, -1, 1);
+		player2.direction.y = clamp(player2.direction.y, -1, 1);
 
 	}
 	return (0);
@@ -133,18 +171,16 @@ int update(t_cub *cub);
 
 void init_players()
 {
-	player.pos = (t_point){ 20, SCREEN_SIZE_Y / 2 };
+	hit_right = true;
+	hit_left = true;
+	player.pos = (t_point){ offset, SCREEN_SIZE_Y / 2 };
 	player.direction = (t_point){0, 0};
-	player2.pos = (t_point){SCREEN_SIZE_X - 40, SCREEN_SIZE_Y / 2};
+	player2.pos = (t_point){SCREEN_SIZE_X - (offset + padsize.x), SCREEN_SIZE_Y / 2};
 	player2.direction = (t_point){0, 0};
-	ball.pos = (t_point){SCREEN_SIZE_X / 2, SCREEN_SIZE_Y / 2};
-	ball.direction = (t_point){-1, 0.76};
-	ball.velocity.x = ball.direction.x * ball_speed;
-	ball.velocity.y = ball.direction.y * ball_speed;
+	ball.pos = (t_point){SCREEN_SIZE_X / 2 - (ballsize.x / 2), SCREEN_SIZE_Y / 2 - (ballsize.y / 2)};
 }
 int	main(int ac, char **av)
 {
-	t_cub	cub;
 	(void)av; (void) ac;
 	srand(time(NULL));
 	init_window(&cub);
@@ -213,13 +249,23 @@ void	move()
 
 }
 
+void	print_score()
+{
+	mlx_string_put(cub.mlx_ptr, cub.win_ptr, SCREEN_SIZE_X / 2 - offset, offset, 0xFFFFFF, score_1);
+	mlx_string_put(cub.mlx_ptr, cub.win_ptr, SCREEN_SIZE_X / 2 + offset, offset, 0xFFFFFF, score_2);
+	if (score_2[0] == '5')
+		mlx_string_put(cub.mlx_ptr, cub.win_ptr, SCREEN_SIZE_X / 2 - offset, SCREEN_SIZE_Y / 2 - 100, 0xFFFFFF, "Player 2 won!");
+	if (score_1[0] == '5')
+		mlx_string_put(cub.mlx_ptr, cub.win_ptr, SCREEN_SIZE_X / 2 - offset, SCREEN_SIZE_Y / 2 - 100, 0xFFFFFF, "Player 1 won!");
+}
+
 int renderer(t_cub *cub)
 {
-	calculate_Delta();
 	cub->image.img = mlx_new_image(cub->mlx_ptr, SCREEN_SIZE_X, SCREEN_SIZE_Y);
 	cub->image.addr = mlx_get_data_addr(cub->image.img, &cub->image.bits_per_pixel,
 			&cub->image.line_length, &cub->image.endian);
 
+	drawrect(&cub->image, (t_point){0, 0}, (t_point){SCREEN_SIZE_X, SCREEN_SIZE_Y}, 0x2E8B47);
 	drawline(cub, (t_point){SCREEN_SIZE_X / 2, 0}, (t_point){SCREEN_SIZE_X / 2, SCREEN_SIZE_Y});
 	drawrect(&cub->image, player.pos, padsize, 0xFF0000);
 	drawrect(&cub->image, player2.pos, padsize, 0x00FF00);
@@ -227,32 +273,67 @@ int renderer(t_cub *cub)
 
 
 	mlx_put_image_to_window(cub->mlx_ptr, cub->win_ptr, cub->image.img, 0, 0);
+	print_score();
 	mlx_destroy_image(cub->mlx_ptr, cub->image.img);
 	return (0);
 }
 
+void	hit_player()
+{
+	if (hit_left && ball.pos.x > offset && ball.pos.x <= offset + padsize.x)
+	{
+		if (ball.pos.y >= player.pos.y && ball.pos.y <= player.pos.y + padsize.y)
+		{
+			hit_left = false;
+			hit_right = true;
+			ball.direction.y = (float)rand() / (float)RAND_MAX * 1.4f - 0.7f;
+			ball.direction.x = -ball.direction.x;
+			ball_speed += ball_speed * 0.05;
+			ball.direction = (t_point)normalize(ball.direction);
+		}
+	}
+	else if (hit_right && ball.pos.x + ballsize.x <= SCREEN_SIZE_X - offset && ball.pos.x + ballsize.x >= SCREEN_SIZE_X - offset - padsize.x)
+	{
+		if (ball.pos.y >= player2.pos.y && ball.pos.y <= player2.pos.y + padsize.y)
+		{
+			hit_left = true;
+			hit_right = false;
+			ball.direction.y = (float)rand() / (float)RAND_MAX * 1.4f - 0.7f;
+			ball.direction.x = -ball.direction.x;
+			ball_speed += ball_speed * 0.05;
+			ball.direction = (t_point)normalize(ball.direction);
+		}
+	}
+}
+
 void	move_ball()
 {
+	hit_player();
 	if (ball.pos.x < 0)
 	{
+		started = false;
+		//ball.pos = (t_point){SCREEN_SIZE_X / 2, SCREEN_SIZE_Y / 2};
+		score_2[0] += 1;
+		init_players();
 		ball.direction.x = -ball.direction.x;
 	}
-	if (ball.pos.x > SCREEN_SIZE_X - ballsize.x)
+	else if (ball.pos.x > SCREEN_SIZE_X - ballsize.x)
 	{
+		started = false;
+		ball.pos = (t_point){SCREEN_SIZE_X / 2, SCREEN_SIZE_Y / 2};
+		score_1[0] += 1;
+		init_players();
 		ball.direction.x = -ball.direction.x;
-		ball.pos.x = SCREEN_SIZE_X - ballsize.x;
 	}
-	if (ball.pos.y < 0)
+	else if (ball.pos.y < 0)
 	{
 		ball.direction.y = -ball.direction.y;
 	}
-	if (ball.pos.y > SCREEN_SIZE_Y - ballsize.y)
+	else if (ball.pos.y > SCREEN_SIZE_Y - ballsize.y)
 	{
 		ball.direction.y = -ball.direction.y;
 		ball.pos.y = SCREEN_SIZE_Y - ballsize.y;
 	}
-
-
 	ball.pos.x += ball.direction.x * ball_speed *  delta;
 	ball.pos.y += ball.direction.y * ball_speed * delta;
 
@@ -273,6 +354,5 @@ int	game_loop(t_cub *cub)
 {
 	update(cub);
 	renderer(cub);
-	started = true;
 	return (0);
 }
