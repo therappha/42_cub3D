5db93d975713f3263a7c8cd7653df41d3db4fb9a
larocqueg <portugalgabriel2021@gmail.com>
2025-06-01 17:29:29 +0100

    norminette

--- srcs/debug.c
+++ srcs/debug.c
@@ -22,13 +22,17 @@ void	print_fps(t_cub *cub)
 
 void	debug_directions(t_cub *cub)
 {
-	t_point normalized_player = normalize(cub->player.direction);
-	drawline(cub, (t_point){WIDTH / 4, HEIGHT -  (HEIGHT / 4)}, (t_point){WIDTH / 4 + normalized_player.x * CIRCLE_SIZE,  HEIGHT -  (HEIGHT / 4) + normalized_player.y * CIRCLE_SIZE});
-	drawcircle(cub, WIDTH / 4, HEIGHT -  (HEIGHT / 4), CIRCLE_SIZE + 1);
-	drawcircle(cub, WIDTH / 4, HEIGHT -  (HEIGHT / 4), CIRCLE_SIZE);
+	t_point	normalized_player;
 
-	drawline(cub, (t_point){WIDTH - WIDTH / 4, HEIGHT -  (HEIGHT / 4)}, (t_point){WIDTH - WIDTH / 4 + cub->player.camera.x * CIRCLE_SIZE,  HEIGHT -  (HEIGHT / 4) + cub->player.camera.y * CIRCLE_SIZE});
-	drawcircle(cub, WIDTH -  WIDTH / 4, HEIGHT -  (HEIGHT / 4), CIRCLE_SIZE);
-	drawcircle(cub, WIDTH -  WIDTH / 4, HEIGHT -  (HEIGHT / 4), CIRCLE_SIZE + 1);
+	normalized_player = normalize(cub->player.direction);
+	drawline(cub, (t_point){WIDTH / 4, HEIGHT - (HEIGHT / 4)},
+		(t_point){WIDTH / 4 + normalized_player.x * CIRCLE_SIZE,
+		HEIGHT - (HEIGHT / 4) + normalized_player.y * CIRCLE_SIZE});
+	drawcircle(cub, WIDTH / 4, HEIGHT - (HEIGHT / 4), CIRCLE_SIZE + 1);
+	drawcircle(cub, WIDTH / 4, HEIGHT - (HEIGHT / 4), CIRCLE_SIZE);
+	drawline(cub, (t_point){WIDTH - WIDTH / 4, HEIGHT - (HEIGHT / 4)},
+		(t_point){WIDTH - WIDTH / 4 + cub->player.camera.x * CIRCLE_SIZE,
+		HEIGHT - (HEIGHT / 4) + cub->player.camera.y * CIRCLE_SIZE});
+	drawcircle(cub, WIDTH - WIDTH / 4, HEIGHT - (HEIGHT / 4), CIRCLE_SIZE);
+	drawcircle(cub, WIDTH - WIDTH / 4, HEIGHT - (HEIGHT / 4), CIRCLE_SIZE + 1);
 }
-
